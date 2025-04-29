58eedca7e00bb8340879a7bab2564056f33f4e1b
Julia Polyakova <Iu.A.Poliakova@gmail.com>
2025-04-29 19:12:41 +0500

    feat: add env, jwt decode and cookie service

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    apiUrl: 'http://localhost:8080',
+    TOKEN_NAME: 'accessToken',
+    LOGIN_TIMESTAMP_NAME: 'loginTimestamp',
+    TOKEN_LIFESPAN: 24 * 60 * 60 * 1000,
+};
