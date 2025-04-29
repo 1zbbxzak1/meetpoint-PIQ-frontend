8202404d08a9ff548d9a0b4be744bb61c50d1764
Julia Polyakova <Iu.A.Poliakova@gmail.com>
2025-04-29 19:21:50 +0500

    feat: add providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,30 @@
-import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {ApplicationConfig, LOCALE_ID, provideZoneChangeDetection} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideClientHydration, withEventReplay} from '@angular/platform-browser';
+import {HTTP_INTERCEPTORS, provideHttpClient} from '@angular/common/http';
+import {JwtTokenInterceptor} from './interceptors/jwt-token.interceptor';
+import {AuthService} from './data/services/auth/auth.service';
+import {AuthManagerService} from './data/services/auth/auth.manager.service';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideZoneChangeDetection({eventCoalescing: true}), provideRouter(routes), provideClientHydration(withEventReplay())]
+    providers: [
+        provideZoneChangeDetection({eventCoalescing: true}),
+        provideRouter(routes),
+        provideClientHydration(withEventReplay()),
+        provideHttpClient(),
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: JwtTokenInterceptor,
+            multi: true
+        },
+        {
+            provide: LOCALE_ID,
+            useValue: 'ru'
+        },
+
+        AuthService,
+        AuthManagerService,
+    ]
 };
