275d33767e16f5710302e9de3610e794ce0f7035
Julia Polyakova <Iu.A.Poliakova@gmail.com>
2025-06-07 17:22:10 +0500

    refactor: add new ip address for backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    apiUrlPIQ: 'http://81.31.244.118:8080',
-    apiUrlAccount: 'http://81.31.244.118:5247',
+    apiUrlPIQ: 'http://158.160.45.151:8080',
+    apiUrlAccount: 'http://158.160.45.151:5247',
     TOKEN_NAME: 'accessToken',
 };
